5ce692a2b841305c93700823b416c14d0a69a21a
1MisSim1 <132581321+1MisSim1@users.noreply.github.com>
2023-05-04 15:42:46 +0200

    Add files via upload

---
+++ strings/main.py
@@ -0,0 +1,22 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+scorer_1 = 'Ruud Gullit '
+scorer_2 = 'Marco van Basten '
+goal_0 = 32
+goal_1 = 54
+scorers = scorer_1  + str(goal_0), scorer_2  + str(goal_1)
+report = f' {scorer_1} scored in the {goal_0}nd minute ' '\n' + f' {scorer_2} scored in the {goal_1}th minute '
+player = 'Ronald Koeman'
+first_name = player.find("Ronald")
+first_name_isolate=player[:6]
+last_name_isolate=player[7:]
+last_name=player[7:]
+last_name_len=last_name
+print(len(last_name))
+name_short= f'R. {last_name} '
+first_name_isolate + '!'
+chant=first_name_isolate + '!'
+good_chant=chant * 3
